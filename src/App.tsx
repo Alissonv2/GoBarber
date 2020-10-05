13b9d3acb29b2ebcd7ba444d632d17d6e636a1c4
alisson <alissonvitorino8@gmail.com>
2020-10-05 10:20:52 -0300

    Subindo o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AuthProvider } from './context/AuthContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Signin from './pages/Signin';
+/* import Signup from './pages/Signup'; */
+import GlobalStyle from './styles/global';
 
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <Signin />
+    </AuthProvider>
+    <GlobalStyle />
+  </>
+);
 export default App;
