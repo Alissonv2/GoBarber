17276c834b16eb74e3835acd9dbc093e216f3307
Alissonv2 <alissonvitorino8@gmail.com>
2020-12-23 20:54:28 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { AuthProvider } from './context/AuthContext';
 
 import Signin from './pages/Signin';
 /* import Signup from './pages/Signup'; */
 import GlobalStyle from './styles/global';
 
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <Signin />
-    </AuthProvider>
+    </AppProvider>
     <GlobalStyle />
   </>
 );
